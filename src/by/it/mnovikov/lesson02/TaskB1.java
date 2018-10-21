c81e76313a0818ff396a46432df1e70e86963652
n00bsya <n00bsya@mail.ru>
2018-10-21 15:23:13 +0300

    lesson_02

--- src/by/it/mnovikov/lesson02/TaskB1.java
+++ src/by/it/mnovikov/lesson02/TaskB1.java
@@ -19,6 +19,16 @@ int i=sc.nextInt();
 
  */
 
+import java.util.Scanner;
+
 class TaskB1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("квадрат числа " + a + " равен " + a*a);
+
+    }
+
 
 }
