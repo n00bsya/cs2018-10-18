048489c9e6ee886a4458f0618143929479083955
n00bsya <n00bsya@mail.ru>
2018-10-21 16:17:16 +0300

    lesson_02

--- src/by/it/mnovikov/lesson02/TaskC1.java
+++ src/by/it/mnovikov/lesson02/TaskC1.java
@@ -10,10 +10,20 @@ package by.it.mnovikov.lesson02;
 Sum = 60
 
 */
+
+
+import java.util.Scanner;
+
 class TaskC1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число а");
+        int a = sc.nextInt();
+        System.out.println("Введите число b");
+        int b = sc.nextInt();
 
+        int Sum = a + b;
 
-
-
-
+        System.out.println("Sum = " + Sum);
+    }
 }
--- src/by/it/mnovikov/lesson02/TaskC2.java
+++ src/by/it/mnovikov/lesson02/TaskC2.java
@@ -13,7 +13,35 @@ BIN:100010+11010=111100
 HEX:22+1a=3c
 OCT:42+32=74
 */
+
+
+import java.util.Scanner;
+
 class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число а");
+        int a = sc.nextInt();
+        System.out.println("Введите число b");
+        int b = sc.nextInt();
+        int Sum = a + b;
+
+        String binary_a = Integer.toBinaryString(a);
+        String binary_b = Integer.toBinaryString(b);
+        String binary_Sum = Integer.toBinaryString(Sum);
+        String hex_a = Integer.toHexString(a);
+        String hex_b = Integer.toHexString(b);
+        String hex_Sum = Integer.toHexString(Sum);
+        String octal_a = Integer.toOctalString(a);
+        String octal_b = Integer.toOctalString(b);
+        String octal_Sum = Integer.toOctalString(Sum);
+
+        System.out.println("DEC:" + a + "+" + b + "=" + Sum);
+        System.out.println("BIN:"+binary_a+"+"+binary_b+"="+binary_Sum);
+        System.out.println("HEX:"+hex_a+"+"+hex_b+"="+hex_Sum);
+        System.out.println("OCT:"+octal_a+"+"+octal_b+"="+octal_Sum);
+
+    }
 
 
 }
