2c0ec5b2d3ec9bae0009f2b220ab5faa09e6f608
serzh <333antitrust3331973@gmail.com>
2018-10-27 12:25:57 +0300

    lesson_04 A1, A2

--- src/by/it/mnovikov/lesson04/TaskA2.java
+++ src/by/it/mnovikov/lesson04/TaskA2.java
@@ -29,8 +29,8 @@ import java.util.Scanner;
 public class TaskA2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int b =scanner.nextInt();
-        switch (b) {
+        int a =scanner.nextInt();
+        switch (a) {
             case 1: System.out.println("понедельник"); break;
             case 2: System.out.println("вторник"); break;
             case 3: System.out.println("среда"); break;
