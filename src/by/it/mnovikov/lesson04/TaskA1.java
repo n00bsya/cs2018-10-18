f53d2350b832e3c7e8a984caa8666dd99b400713
serzh <333antitrust3331973@gmail.com>
2018-10-27 12:21:52 +0300

    lesson_04 A1, A2

--- src/by/it/mnovikov/lesson04/TaskA1.java
+++ src/by/it/mnovikov/lesson04/TaskA1.java
@@ -28,9 +28,9 @@ public class TaskA1 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             if (i != 2 && i != 8) {
-                System.out.println("Меня интересует высокая зарплата!");
-            } else {
                 System.out.println("Я изучаю программирование.");
+            } else {
+                System.out.println("Меня интересует высокая зарплата!");
             }
         }
     }
