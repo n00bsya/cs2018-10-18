60add49f153a44995eb0cc0b0a1a02621719f1d3
n00bsya <n00bsya@mail.ru>
2018-11-01 00:23:04 +0300

    lesson_04 tasks C3

--- src/by/it/mnovikov/lesson04/TaskC3.java
+++ src/by/it/mnovikov/lesson04/TaskC3.java
@@ -24,5 +24,36 @@ package by.it.mnovikov.lesson04;
 */
 
 public class TaskC3 {
-
+    public static void main(String[] args) {
+        for (int i=2; i<=10; i++ ){
+            for (int j=2; j<=10; j++){
+                switch (i){
+                    case 2: System.out.print("два"); break;
+                    case 3: System.out.print("три"); break;
+                    case 4: System.out.print("четыре"); break;
+                    case 5: System.out.print("пять"); break;
+                    case 6: System.out.print("шесть"); break;
+                    case 7: System.out.print("семь"); break;
+                    case 8: System.out.print("восемь"); break;
+                    case 9: System.out.print("девять"); break;
+                    default: System.out.print("десять"); break;
+            }
+                System.out.print(" умножить на ");
+                switch (j){
+                    case 2: System.out.print("два"); break;
+                    case 3: System.out.print("три"); break;
+                    case 4: System.out.print("четыре"); break;
+                    case 5: System.out.print("пять"); break;
+                    case 6: System.out.print("шесть"); break;
+                    case 7: System.out.print("семь"); break;
+                    case 8: System.out.print("восемь"); break;
+                    case 9: System.out.print("девять"); break;
+                    default: System.out.print("десять"); break;
+                }
+                System.out.print(" равно ");
+                int res = i * j;
+                System.out.println(res);
+            }
+        }
+    }
 }
