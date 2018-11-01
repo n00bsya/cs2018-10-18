d097d107b951796f6a4b8eb706765f68ef74cbfc
n00bsya <n00bsya@mail.ru>
2018-11-01 17:03:22 +0300

    Lesson_04 Task C3

--- src/by/it/mnovikov/lesson04/TaskC3.java
+++ src/by/it/mnovikov/lesson04/TaskC3.java
@@ -25,35 +25,51 @@ package by.it.mnovikov.lesson04;
 
 public class TaskC3 {
     public static void main(String[] args) {
+
         for (int i=2; i<=10; i++ ){
             for (int j=2; j<=10; j++){
-                switch (i){
-                    case 2: System.out.print("два"); break;
-                    case 3: System.out.print("три"); break;
-                    case 4: System.out.print("четыре"); break;
-                    case 5: System.out.print("пять"); break;
-                    case 6: System.out.print("шесть"); break;
-                    case 7: System.out.print("семь"); break;
-                    case 8: System.out.print("восемь"); break;
-                    case 9: System.out.print("девять"); break;
-                    default: System.out.print("десять"); break;
-            }
+                MnConvertToString (i);
                 System.out.print(" умножить на ");
-                switch (j){
-                    case 2: System.out.print("два"); break;
-                    case 3: System.out.print("три"); break;
-                    case 4: System.out.print("четыре"); break;
-                    case 5: System.out.print("пять"); break;
-                    case 6: System.out.print("шесть"); break;
-                    case 7: System.out.print("семь"); break;
-                    case 8: System.out.print("восемь"); break;
-                    case 9: System.out.print("девять"); break;
-                    default: System.out.print("десять"); break;
-                }
+                MnConvertToString (j);
                 System.out.print(" равно ");
                 int res = i * j;
+//                ResConvertToString(res);
                 System.out.println(res);
             }
         }
     }
+
+    private static void MnConvertToString(int a) {
+        switch (a){
+            case 2: System.out.print("два"); break;
+            case 3: System.out.print("три"); break;
+            case 4: System.out.print("четыре"); break;
+            case 5: System.out.print("пять"); break;
+            case 6: System.out.print("шесть"); break;
+            case 7: System.out.print("семь"); break;
+            case 8: System.out.print("восемь"); break;
+            case 9: System.out.print("девять"); break;
+            default: System.out.print("десять"); break;
+        }
+    }
+
+/*    public static void ResConvertToString(int res) {
+        int ost = res - (res/10)*10;
+        int dstk = res - ost;
+        if (dstk > 19 & dstk <= 100) {
+            switch (dstk){
+                case 20: System.out.print("двадцать"); break;
+                case 30: System.out.print("тридцать"); break;
+                case 40: System.out.print("сорок"); break;
+                case 50: System.out.print("пятьдесят"); break;
+                case 60: System.out.print("шестьдесят"); break;
+                case 70: System.out.print("семьдесят"); break;
+                case 80: System.out.print("восемьдесят"); break;
+                case 90: System.out.print("девяносто"); break;
+                default: System.out.print("сто"); break;
+            }
+            else */
 }
+//    }
+
+//}
