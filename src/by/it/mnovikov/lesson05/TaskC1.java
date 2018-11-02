76997c547d87975bcf90700f77969edce38e3320
n00bsya <n00bsya@mail.ru>
2018-11-02 13:36:29 +0300

    Lesson_05 Task C1

--- src/by/it/mnovikov/lesson05/TaskC1.java
+++ src/by/it/mnovikov/lesson05/TaskC1.java
@@ -10,21 +10,38 @@ package by.it.mnovikov.lesson05;
     Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
 
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TaskC1 {
-    public static void main(String[] args) throws Exception
-    {
-        int [] list = new int [20];
-        Scanner scan = new Scanner (System.in);
+    public static void main(String[] args) throws Exception {
+        int[] list = new int[20];
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<Integer> d3 = new ArrayList<>();
+        ArrayList<Integer> d2 = new ArrayList<>();
+        ArrayList<Integer> ost = new ArrayList<>();
 
         for (int i = 0; i < list.length; i++) {
-            list[i] = scan.nextInt();
+            list [i] = scan.nextInt();
         }
-        
-
+        for (int x : list) {
+            if (x % 3 == 0 & x % 2 == 0) {
+                d3.add(x);
+                d2.add(x);
+            } else if (x % 3 == 0) {
+                d3.add(x);
+            } else if (x % 2 == 0) {
+                d2.add(x);
+            } else ost.add(x);
+        }
+        printList(d3);
+        printList(d2);
+        printList(ost);
     }
+
 
     private static void printList(List<Integer> list) {
         for (Integer aList : list) System.out.println(aList);
@@ -32,3 +49,5 @@ public class TaskC1 {
 
 
 }
+
+//1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
