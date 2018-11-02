47ad119a508400ea8d08089fdea4edf5f97cf83b
n00bsya <n00bsya@mail.ru>
2018-11-02 14:35:08 +0300

    Lesson_05 Task C2 (bubbles)

--- src/by/it/mnovikov/lesson05/TaskC2.java
+++ src/by/it/mnovikov/lesson05/TaskC2.java
@@ -4,15 +4,32 @@ package by.it.mnovikov.lesson05;
 Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
 */
 
-public class TaskC2 {
-    //Напишите тут ваш код
+import java.util.Scanner;
 
+public class TaskC2 {//Напишите тут ваш код
 
+    public static void main(String[] args) {
+        int[] numbers = new int[20];
+        Scanner scan = new Scanner(System.in);
 
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scan.nextInt();
+        }
+        sort(numbers);
+        for (int i : numbers) {
+            System.out.println(i);
+        }
 
-
-    public static void sort(int[] array) {
-        //Напишите тут ваш код
     }
 
-}
+    public static void sort(int[] array) {//Напишите тут ваш код
+        for (int i=0; i<array.length; i++)
+            for (int j=0; j<array.length-i-1; j++)
+        if (array[j]<array[j+1]) {
+            int tmp = array[j];
+            array[j]=array[j+1];
+            array[j+1]=tmp;
+        }
+        }
+    }
+//23 56 74 56 98 45 68 45 2 3 45 77 56 34 27 35 24 28 55 78
