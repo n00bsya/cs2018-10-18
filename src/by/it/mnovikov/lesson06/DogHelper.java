f58736551a4c8a0572bbe1ba1975d00332f7867d
akoltovich <alexeyugol@gmail.com>
2018-11-03 12:23:23 +0300

    Lesson_06 Tasks A1, A2, B1

---
+++ src/by/it/mnovikov/lesson06/DogHelper.java
@@ -0,0 +1,21 @@
+package by.it.mnovikov.lesson06;
+
+import jdk.internal.dynalink.beans.StaticClass;
+
+public class DogHelper {
+
+    static void printAllNames (Dog[] dogs) {
+        for (Dog dog : dogs) {
+            System.out.print(dog.getName()+ " ");
+            System.out.println();
+        }
+
+    }
+    static double averageAge (Dog[] dogs) {
+        double sum = 0;
+        for (Dog dog : dogs) {
+            sum +=dog.getAge();
+        }
+        return sum/dogs.length;
+    }
+}
